0370837f8584fb07806b4fa2677beb2ee02426da
Fabiano <fabiano.araujodeveloper@gmail.com>
2025-03-16 23:10:23 -0300

    Changing menu

--- code/Menu.py
+++ code/Menu.py
@@ -18,8 +18,8 @@ class Menu:
         pygame.mixer_music.play(-1)  # -1 para que a música fique tocando em loop.
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
-            self.menu_text(50, "Mountain", COLOR_ORANGE, ((WIN_WIDTH / 2), 70))
-            self.menu_text(50, "Shooter", COLOR_ORANGE, ((WIN_WIDTH / 2), 120))
+            self.menu_text(50, "Mountain", COLOR_WHITE, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "Shooter", COLOR_WHITE, ((WIN_WIDTH / 2), 120))
 
             for i in range(len(MENU_OPTION)):
                 self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
