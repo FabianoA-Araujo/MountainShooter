0fca613f1d56ec68086d0ae2ded3163ba6e6fd04
Fabiano <fabiano.araujodeveloper@gmail.com>
2025-03-18 16:31:28 -0300

    Hit Box Collisions HUD Score

--- code/Player.py
+++ code/Player.py
@@ -24,6 +24,7 @@ class Player(Entity):
         if pressed_key[PLAYER_KEY_RIGHT[self.name]] and self.rect.right < WIN_WIDTH:
             self.rect.centerx += ENTITY_SPEED[self.name]
         pass
+
     def shoot(self):
         self.shot_delay -= 1
         if self.shot_delay == 0:
@@ -31,3 +32,7 @@ class Player(Entity):
             pressed_key = pygame.key.get_pressed()
             if pressed_key[PLAYER_KEY_SHOOT[self.name]]:
                 return PlayerShot(name=f'{self.name}Shot', position=(self.rect.centerx, self.rect.centery))
+            else:
+                return None
+        else:
+            return None
